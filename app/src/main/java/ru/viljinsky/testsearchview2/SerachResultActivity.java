de57bb0f9e4950ccc547b019599586c407e0050e
viljinsky <viljinsky@narod.ru>
2017-04-17 20:06:19 +0300

    Rejected commit: Default

---
+++ app/src/main/java/ru/viljinsky/testsearchview2/SerachResultActivity.java
@@ -0,0 +1,33 @@
+package ru.viljinsky.testsearchview2;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by viljinsky on 17.04.2017.
+ */
+
+public class SerachResultActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_result);
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            //use the query to search your data somehow
+        }
+    }
+}
